6fa00d65f9bfccd83aa41d78fa814ba8e65cbcc5
farhad-sofi <Sofizadafarhad@gmail.com>
2025-05-14 21:47:31 -0700

    Update Particles.h

--- Particles/Particles/Particles.h
+++ Particles/Particles/Particles.h
@@ -9,6 +9,13 @@ const float TTL = 5.0; //Time To Live
 const float SCALE = 0.999;
 using namespace Matrices;
 using namespace sf;
+
+enum ParticleType
+{
+	Normal,
+	Spiral
+};
+
 class Particle : public Drawable
 {
 public:
@@ -19,7 +26,14 @@ public:
 	//Functions for unit testing
 	bool almostEqual(double a, double b, double eps = 0.0001);
 	void unitTests();
-private:
+	Vector2f getCenter() const { return m_centerCoordinate; }
+
+	static ParticleType mode;
+
+
+	
+	
+private: 
 	float m_ttl;
 	int m_numPoints;
 	Vector2f m_centerCoordinate;
@@ -30,13 +44,13 @@ private:
 	Color m_color1;
 	Color m_color2;
 	Matrix m_A;
-	///rotate Particle by theta radians counter-clockwise
-	///construct a RotationMatrix R, left mulitply it to m_A
+
+
 	void rotate(double theta);
-	///Scale the size of the Particle by factor c
-	///construct a ScalingMatrix S, left multiply it to m_A
+
 	void scale(double c);
-	///shift the Particle by (xShift, yShift) coordinates
-	///construct a TranslationMatrix T, add it to m_A
+
 	void translate(double xShift, double yShift);
+	Vector2f getMappedMousePosition() const;
+	void applyExtraCredit(float dt);
 };
